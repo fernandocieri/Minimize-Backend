d1b425ee214cd9e34f89d3de8243805a0f8fc29e
fernandocieri <fernandocieri@gmail.com>
2022-04-15 19:27:20 +0200

    Fixes newUrl format

--- managers/url.js
+++ managers/url.js
@@ -6,7 +6,7 @@ mongoose.connect('mongodb://localhost/Minimize'), {
 }
 
 function formatUrl(customName, shortenedUrl) {
-    const newUrl = `http://localhost:3003/api/${customName}/${shortenedUrl}`;
+    const newUrl = `http://localhost:3003/api/url/${customName}/${shortenedUrl}`;
     return newUrl;
 }
 
@@ -23,26 +23,32 @@ class UrlManager {
 
     static async getOriginalUrl(customName, shortenedUrl) {
         try {
-            const originalUrl = await Url.findOne({ shortenedUrl: shortenedUrl });
-            return originalUrl;
+            const urlInfo = await Url.findOne({ shortenedUrl: shortenedUrl });
+            return urlInfo.originalUrl;
         } catch (error) {
             console.log(error);
         }
+    }
+
+    static async getUrlClicks(shortenedUrl) {
+        const urlInfo = await Url.findOne({ shortenedUrl: shortenedUrl });
+        return urlInfo.clicks;
     }
 
     static async getShortenedUrl(originalUrl) {
         try {
-            const shortenedUrl = await Url.findOne({ originalUrl: originalUrl });
-            return shortenedUrl;
+            const urlInfo = await Url.findOne({ originalUrl: originalUrl });
+            return urlInfo.shortenedUrl;
         } catch (error) {
             console.log(error);
         }
     }
 
+
     static async shortenUrl(customName, originalUrl) {
         try {
-            const response = await Url.create({ originalUrl: originalUrl, customName: customName });
-            const shortenedUrl = formatUrl(response.customName, response.shortenedUrl);
+            const createdUrlInfo = await Url.create({ originalUrl: originalUrl, customName: customName });
+            const shortenedUrl = formatUrl(createdUrlInfo.customName, createdUrlInfo.shortenedUrl);
             return shortenedUrl;
         } catch (error) {
             console.log(error);
