bd910b5debc5ba47d3261aedf0e4d751631bb9a8
fernandocieri <fernandocieri@gmail.com>
2022-05-13 20:57:44 +0200

    Debugging

--- controllers/url/getClicks.js
+++ controllers/url/getClicks.js
@@ -2,7 +2,6 @@ const urlManager = require('../../managers/url');
 
 async function getClicks(req, res) {
     const clicksTotal = await urlManager.getUrlClicks(req.params.shortenedUrl);
-    
     if (clicksTotal >= 0) {
         res.status(200).json(clicksTotal);
     } else res.status(404).end();
--- controllers/url/getOriginalUrl.js
+++ controllers/url/getOriginalUrl.js
@@ -1,11 +1,10 @@
 const urlManager = require("../../managers/url");
 
 async function getOriginalUrl(req, res) {
-  const originalUrl = await urlManager.getOriginalUrl(req.params.customName, req.params.shortenedUrl); 
-
+  const originalUrl = await urlManager.getOriginalUrl(req.params.shortenedUrl);
   if (originalUrl) {
-    res.redirect(originalUrl);
-  } else res.status(404).end();
+    res.status(200).json(originalUrl);
+  } else { res.status(404).end(); }
 }
 
 module.exports = getOriginalUrl;
